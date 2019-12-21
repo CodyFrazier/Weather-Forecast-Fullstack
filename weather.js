d8b02ddf944f5083e70a1f86aeda7b55888b435e
Cody Frazier <codyfrazier@me.com>
2019-12-20 18:56:55 -0800

    initial commit

---
+++ weather.js
@@ -0,0 +1,9 @@
+// `api.openweathermap.org/data/2.5/weather?id=`
+// 622cc29a246cffc4f2e0b9446ab8c15d
+
+function testFunct(){
+	console.log('test');
+	document.querySelector('body').innerHTML += `<p>Test Text</p>`;
+}
+
+testFunct();
