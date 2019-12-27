075e4a80423a1dac56fc464d8b509ccb1d0c8350
Cody Frazier <codyfrazier@me.com>
2019-12-27 14:14:12 -0800

    removed temp data. significant alterations to js file

--- weather.js
+++ weather.js
@@ -1,8 +1,4 @@
-// `api.openweathermap.org/data/2.5/weather?`
-	//id: 		+= `id=${ city id }`
-	//zip:		+= `zip=${ city zip }`
-	//coords:	+= `lat=${ latitude }&lon=${ longitude }`
-// 622cc29a246cffc4f2e0b9446ab8c15d
+//units: K [ default ], C [ units=metric ], F [ units=imperial] 
 
 //To Do:
 /*
@@ -17,28 +13,83 @@
 			//accumulated precipitation
 			//air temperature
 	
-	//Weather Search:
-		//Radio Buttons to specify type
-			//By Place Name (city)
-			//By Zip
-			//By Lat and Longitude
-	
 	//True goal:
 		//Make it pleasant to use
 		//Make it easy to read
 */
 
-function buildWeatherCard(id, cardName, info){
+
+const appId = '622cc29a246cffc4f2e0b9446ab8c15d';
+
+async function searchSubmit(){
+	const scale = document.querySelector('#tempScale').value === 'C°' ? '&units=metric'
+	: document.querySelector('#tempScale').value === 'F°' ? '&units=imperial'
+	: '';
+	const searchType = [...document.querySelectorAll('#searchType input')].filter(radio => { return radio.checked })[0].value;
+	const searchString = `${ document.getElementById('searchBarOne').value }${ searchType === 'lat' ? '&' : ',' }${ searchType === 'lat' ? 'lon=' : '' }${ document.getElementById('searchBarTwo').value.toLowerCase() }${ scale }`;
+	console.log(`searchType: ${ searchType }; searchString: ${ searchString }`);
+	const fullStr = `https://api.openweathermap.org/data/2.5/weather?${ searchType }=${ searchString }&APPID=${ appId }`;
+	if(fullStr.length > 90){
+		const response = await fetch(fullStr);
+		const str = await response.json();
+		buildWeatherCard(str);
+	}
 	
-	console.log(info,':', data[info]);
-	document.querySelector('#container').innerHTML += `
-	<div id = ${ id } class = 'weatherCard columnWrap'>
-	<h3>${ cardName }</h3>
-	<div id = ${ info } class = 'infoCard columnWrap'>${ info }: ${ data[info] }</div>
-	</div>
-	`;
 }
 
-for(let i = 1; i <= 10; i++){
-	buildWeatherCard(`card${ i }`, `card${ i }`, `visibility`);
+function buildWeatherCard(apiResponse){
+	document.querySelector('#container').innerHTML += `
+	<div id = "airTemp" class = 'weatherCard columnWrap'>
+	<h3>Air Temperature</h3>
+	<div class = 'infoCard columnWrap largeFont'>${ Math.round(apiResponse.main.temp) }</div>
+	</div>
+	<div id = "hourlyCard" class = 'weatherCard columnWrap'>
+	<h3>Hourly Forecast</h3>
+	<div class = 'infoCard columnWrap'>Graph of Daily Data</div>
+	</div>
+	<div id = "precipRate" class = 'weatherCard columnWrap'>
+	<h3>Precipitation Intensity</h3>
+	<div class = 'infoCard columnWrap'>Rainfall per given time</div>
+	</div>
+	<div id = "precipAmount" class = 'weatherCard columnWrap'>
+	<h3>Accumulated Precipitation</h3>
+	<div class = 'infoCard columnWrap'>Amount of Preciptation Accumulated</div>
+	</div>
+	`;	
 }
+
+document.querySelector('#searchType').addEventListener('click', ({ target }) => { //alter text input fields to reflect the possible values.
+	const bar1 = document.querySelector('#searchBarOne');
+	const bar2 = document.querySelector('#searchBarTwo');
+	document.querySelectorAll('#searchType input').forEach(radio => {
+		if(radio.checked){
+			
+			if(radio.value === 'lat'){
+				bar2.attributes.type = 'number';
+				bar2.attributes.max = '180';
+				bar2.attributes.min = '-180';
+				bar2.removeAttribute('maxlength');
+			}
+			else{
+				bar2.attributes.type = 'text';
+				bar2.removeAttribute('max');
+				bar2.removeAttribute('min');
+				bar2.attributes.maxlength = '2';
+			}
+			console.log(radio.value); //When a search is submitted, use the value here.
+		}
+	});
+});
+
+document.querySelector('.scaleRow').addEventListener('click', ({ target }) => {
+	console.log('target:', target);
+	document.querySelectorAll('.scaleRow input').forEach(button => {
+		if(button.id === 'tempScale'){
+			button.id = '';
+		}
+	});
+	target.id = 'tempScale';
+});
+
+document.querySelector('#submitWrapper').addEventListener('click', searchSubmit);
+
